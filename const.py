3d939afeb5d42ded297e8fa14fc51f0ef7fac50b
Jakukyo Friel <weakish@gmail.com>
2012-09-26 16:15:35 +0800

    make const.py compatible with python 3

--- const.py
+++ const.py
@@ -1,25 +1,30 @@
 # adapted from http://code.activestate.com/recipes/65207-constants-in-python/
 
+'''Why Python doesn't have constants?
+
+
+Usage:
+
+    import const
+    # and bind an attribute ONCE:
+    const.magic = 23
+    # but NOT re-bind it:
+    const.magic = 88      # raises const.ConstError
+    # you may also want to add the obvious __delattr__
+
+Note:
+
+The original version from activestate doesn't work for python 3.
+This version should work on python 3 and (not very early) python 2.
+I've tested it on python 2.6 and python 3.1.
+'''
+
 class _const:
     class ConstError(TypeError): pass
     def __setattr__(self,name,value):
-        if self.__dict__.has_key(name):
-            raise self.ConstError, "Can't rebind const(%s)"%name
+        if name in self.__dict__:
+            raise self.ConstError("Can't rebind const(%s)"%name)
         self.__dict__[name]=value
 import sys
 sys.modules[__name__]=_const()
 
-'''Why Python doesn't have constants?
-
-This does not work for python3.
-
-Usage:
-
-# that's all -- now any client-code can
-import const
-# and bind an attribute ONCE:
-const.magic = 23
-# but NOT re-bind it:
-const.magic = 88      # raises const.ConstError
-# you may also want to add the obvious __delattr__
-'''
